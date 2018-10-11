b6d87a163eaf64b6ef7348d21814f253b0d4f729
saktija <saktija@cgns-MacBook-Air.local>
2018-10-11 22:03:55 +0530

    path change

--- src/app/services/Registration.js
+++ src/app/services/Registration.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 // import config from '../config/apiConfig'
 
-// const commonPath = 'http://103.235.104.177:9000/'
-const commonPath = 'http://localhost:9000/'
+const commonPath = 'http://103.235.104.177:9000/'
+//const commonPath = 'http://localhost:9000/'
 
 export function getService (serviceName) {
     const getUrl = commonPath + serviceName
