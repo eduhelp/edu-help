21cccf01985113c53bf3b16a40bf12e4892bc773
saktija <saktija@cgns-MacBook-Air.local>
2018-10-14 06:24:25 +0530

    level check

--- server/placements.js
+++ server/placements.js
@@ -111,6 +111,7 @@ async function getMyParentLevelWise(user_id, maxLevel, res) {
 router.post('/activeSmartSpreader', async function(req, res) {
     var curQuery = "select * from smart_spreaders t1 left join users t2 on t1.user_id = t2.user_id  where t1.current_status='Active' and t1.payment_level="+req.body.payment_level+" order by t1.spreader_id limit 1"
     var result = await pg_connect.connectDB(curQuery, res)
+    console.log(result)
     if(result) {
         if(result.length > 0) {
             res.status(200).send([{message: 'no smart spreader'}])
