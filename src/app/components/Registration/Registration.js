81fa06184b4d85b0c08efbca71109006b0afea96
saktija <saktija@cgns-MacBook-Air.local>
2018-10-24 17:16:57 +0530

    reg chagnes

--- src/app/components/Registration/Registration.js
+++ src/app/components/Registration/Registration.js
@@ -9,7 +9,6 @@ import Step from '@material-ui/core/Step';
 import StepLabel from '@material-ui/core/StepLabel';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import SponsorEntry from './SponsorEntry'
 import SponsorInfo from './SponsorInfo'
 import UserInfo from './UserInfo'
 import ConfirmInfo from './ConfirmInfo'
